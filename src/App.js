03a68cdce7fe9f8398917223ba80dc290a2686bd
Conrad Forbes <cforbes@captivate.com>
2022-03-29 10:50:07 -0400

    4th commit

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React, Conrad
+          Learn React, Raddish
         </a>
       </header>
     </div>

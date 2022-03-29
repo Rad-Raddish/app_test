eb6bd6026801364ed227e2894816c12f1c8735e1
Conrad Forbes <cforbes@captivate.com>
2022-03-29 11:38:39 -0400

    5th commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
+// this is a component of the app
 function App() {
+
+  // useState hooks cannot be gated and must always be in order that you want to execute them in, so no IF, Functions or loops
+    
+  const [count, setCount] = useState(() => {
+    console.log("run func, this only gets run once")
+    return 4
+  });
+
+  const [theme, setTheme] = useState('blue');
+
+  function decrementCount(){
+    setCount(prevCount => prevCount - 1)
+    setTheme('green');
+  }
+
+  function incrementCount(){
+    setCount(prevCount => prevCount + 1)
+    setTheme('red');
+  }
+
+  // when you use a variable within the HTML object your returning, you have to put it inside of {}
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React, Raddish
-        </a>
-      </header>
+      <button onClick={decrementCount}>-</button>
+      <span>{count}</span>
+      <span>{theme}</span>
+      <button onClick={incrementCount}>+</button>
     </div>
   );
 }
